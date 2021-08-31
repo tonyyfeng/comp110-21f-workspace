45fd103a23dead90256dcb35b0c77dc085640eea
Tony Feng <tyfeng@email.unc.edu>
2021-08-30 22:20:29 -0400

    Finished LS01

--- exercises/ex01/relational_operators.py
+++ exercises/ex01/relational_operators.py
@@ -1,4 +1,4 @@
-"""This program is meant to have the user input two variables and then show the user using those two variables how four of the relational operators work in Python"""
+"""This program is meant to have the user input two variables and then show the user using those two variables how four of the relational operators work in Python."""
 __author__ = "730410711"
 left_number: int = int(input("What is the left-hand side number? (enter an integer) "))
 right_number: int = int(input("What is the right-hand side number? (enter an integer) "))
