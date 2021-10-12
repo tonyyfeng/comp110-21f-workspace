438561321ce878d35d3c6756fd5d9b89dc6c47d5
Tony Feng <tyfeng@email.unc.edu>
2021-10-12 16:18:33 -0400

    Finished Ex06

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,61 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730410711"
 
 # Define your functions below
+
+
+def invert(invert: dict[str, str]) -> dict[str, str]:
+    """This function will alter a dictionary so that the keys are the values and the values are the keys."""
+    list_key: list[str] = list()
+    list_value: list[str] = list()
+    for key in invert:
+        list_key.append(key)
+    for value in invert:
+        list_value.append(invert[value])
+    i: int = 0
+    counter: int
+    while i < len(list_value):
+        counter = i + 1
+        while counter < len(list_value):
+            if list_value[counter] == list_value[i]:
+                raise KeyError("The dictionary you provided has duplicate values and thus cannot be reversed!")
+            counter += 1
+        i += 1
+    i = 0
+    new_dictionary: dict[str, str] = dict()
+    while i < len(invert):
+        new_dictionary[list_value[i]] = list_key[i]
+        i += 1
+    return new_dictionary
+
+
+def favorite_color(colors: dict[str, str]) -> str:
+    """Given a dictionary full of names of people and their favorite colors, this function will return the color that is the favorite of the most people."""
+    count_color: dict[str, int] = dict()
+    for key in colors:
+        if colors[key] in count_color:
+            count_color[colors[key]] += 1
+        else:
+            count_color[colors[key]] = 1
+    counter: int = 0
+    for color in count_color:
+        counter = 0
+        for color_compare in count_color:
+            if count_color[color] >= count_color[color_compare]:
+                counter += 1
+                if counter == len(count_color):
+                    return color
+    return "No Color"
+
+
+def count(list_counted: list[str]) -> dict[str, int]:
+    """Given a list of strings, return a dictionary with how many times elements in that list are repeated."""
+    counter: dict[str, int] = dict()
+    for item in list_counted:
+        if item in counter:
+            counter[item] += 1
+        else:
+            counter[item] = 1
+    return counter
+
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,61 @@
 """Unit tests for dictionary functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
+from exercises.ex06.dictionaries import invert, favorite_color, count
 
-__author__ = "123456789"
+
+__author__ = "730410711"
+
+
+def test_invert_edge() -> None:
+    """Tests an empty dictionary."""
+    xs: dict[str, str] = dict()
+    assert invert(xs) == {}
+
+
+def test_invert_normal_one() -> None:
+    """Tests a normal use case of strings and strings."""
+    xs: dict[str, str] = {"hi": "yo", "boo": "ahhh", "what": "nothing"}
+    assert invert(xs) == {"yo": "hi", "ahhh": "boo", "nothing": "what"}
+
+
+def test_invert_normal_two() -> None:
+    """Tests another normal use case of different strings."""
+    xs:dict[str, str] = {"123": "456", "789": "0", "345": "678"}
+    assert invert(xs) == {"456": "123", "0": "789", "678": "345"}
+
+
+def test_favorite_color_empty_dictionary() -> None:
+    """Tests an empty dictionary"""
+    xs: dict[str, str] = dict()
+    assert favorite_color(xs) == "No Color"
+
+
+def test_favorite_color_blue() -> None:
+    """Tests a scenario where the favorite color is blue."""
+    xs: dict[str, str] = {"asdf": "blue", "hello": "red", "chicken": "green", "bob": "green", "bib": "blue", "yo": "blue"}
+    assert favorite_color(xs) == "blue"
+
+
+def test_favorite_color_tied_colors() -> None:
+    """Tests a scanrio where there are multiple colors with the most amount of people having them as their favorite."""
+    xs: dict[str, str] = {"asdf": "blue", "hello": "red", "chicken": "green", "bob": "green", "bib": "blue", "yo": "red"}
+    assert favorite_color(xs) == "blue"
+
+
+def test_count_empty_list() -> None:
+    """Tests when the input is an empty list."""
+    xs: list[str] = []
+    assert count(xs) == {}
+
+
+def test_count_list_of_one_word() -> None:
+    """Tests a list of strings that are all identical."""
+    xs: list[str] = ["hello", "hello", "hello", "hello", "hello", "hello", "hello"]
+    assert count(xs) == {"hello": 7}
+
+
+def test_count_regular_list() -> None:
+    """Tests a regular string of various lists."""
+    xs: list[str] = ["hello", "yo", "hi", "hello", "hi", "what up", "what up", "ok"]
+    assert count(xs) == {"hello": 2, "yo": 1, "hi": 2, "what up": 2, "ok": 1}
